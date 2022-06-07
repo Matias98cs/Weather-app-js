a2940104631fc8a9de97768bede79ec9291b5fd0
Matias98cs <matias98cs@gmail.com>
2022-06-07 19:25:00 +0200

    4to wea

--- js/mostrarError.js
+++ js/mostrarError.js
@@ -1,16 +1,18 @@
+import { crearHTML, limpiarHTML } from "./funciones.js";
 import { divContainer } from "./selectores.js";
 
 
 export function mostrarError(msj) {
-
     const erorP = document.createElement('p');
     erorP.classList.add('p-error')
     erorP.innerHTML = msj;
+    limpiarHTML();
     divContainer.insertBefore(erorP, divContainer.children[0]);
     // divContainer.appendChild(erorP);
 
     setTimeout(() => {
         erorP.remove()
+        crearHTML()
     }, 3000);
 
 }
