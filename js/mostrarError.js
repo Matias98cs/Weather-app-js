9847cd3f9dcfd391c511f30fe891ba2f982272b1
Matias98cs <matias98cs@gmail.com>
2022-06-07 19:11:33 +0200

    3er wea

--- js/mostrarError.js
+++ js/mostrarError.js
@@ -7,7 +7,6 @@ export function mostrarError(msj) {
     erorP.classList.add('p-error')
     erorP.innerHTML = msj;
     divContainer.insertBefore(erorP, divContainer.children[0]);
-
     // divContainer.appendChild(erorP);
 
     setTimeout(() => {
